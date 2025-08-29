532f65a5c1be93557e9089066cdbd64f3873d72d
Uriel Alfredo <uabotello.dev@gmail.com>
2025-08-28 18:55:17 -0600

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/alfred_uarc7-music",
+  assetPrefix: "/alfred_uarc7-music",
 };
 
 export default nextConfig;
